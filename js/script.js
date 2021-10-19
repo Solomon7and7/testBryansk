42221c41fc2393864757b290c7133c252bb5d21c
Solomon Zakharov <solomon7and@gmail.com>
2021-10-19 16:29:06 +0300

    пк готова переходим к моб

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ const winners = document.querySelector('.winners');
 const infoBox = document.querySelector('.info-box');
 const footer = document.querySelector('.footer');
 
-const author = document.querySelector('.author')
+const author = document.querySelector('.author');
 const authorLogin = document.querySelector('.author__login');
 const authorReg = document.querySelector('.author__reg');
 
 //Для окна авторизации
 const win = document.createElement('div');
 const winClose = document.createElement('div');
-const winButtonClose = document.createElement('div');
+const winButtonClose = document.createElement('button');
 
 // при клике на кнопку, плавно прокручиваем страницу
 rules.addEventListener('click', e => {
@@ -26,24 +26,34 @@ winners.addEventListener('click', e => {
   footer.scrollIntoView({block: "start", behavior: "smooth"});
 });
   
-author.addEventListener('click', createModalWin);
+authorLogin.addEventListener('click', createModalWin);
+authorReg.addEventListener('click', createModalWin);
+authorReg.addEventListener('click', e => {
+  authorReg.classList.add('active')
+})
 
 function createModalWin() {
-  
+
   if (document.querySelector('.win') < 1 ) {
     win.classList.add('win');
     winClose.classList.add('win__close');
     winButtonClose.classList.add('win__button-close');
-  
+    winButtonClose.textContent = 'Закрыть';
+
     author.append(win);
     win.append(winClose);
     win.append(winButtonClose);
 
     winClose.addEventListener('click', () => {
-      // win.style.display = 'none';
-      win.classList.toggle('win');
-    })
+      win.style.display = 'none';
+      authorReg.classList.remove('active')
+    });
+    winButtonClose.addEventListener('click', () => {
+      win.style.display = 'none';
+      authorReg.classList.remove('active')
+    });
+
   } else {
-    win.classList.toggle('win');
-  }
-}
+    win.style.display = 'block';
+  };
+};
