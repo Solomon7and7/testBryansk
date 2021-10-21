e57c9d90a53f5aeb39ca1bd0c5f2d0784785d8d2
Solomon Zakharov <solomon7and@gmail.com>
2021-10-21 04:15:44 +0300

    бургер в процессе приготовления

--- js/script.js
+++ js/script.js
@@ -3,10 +3,13 @@ const prizes = document.querySelector('.prizes');
 const winners = document.querySelector('.winners');
 const infoBox = document.querySelector('.info-box');
 const footer = document.querySelector('.footer');
-
 const author = document.querySelector('.author');
 const authorLogin = document.querySelector('.author__login');
 const authorReg = document.querySelector('.author__reg');
+const burgerMenu = document.querySelector('.burger-menu');
+const burgerMenuItemBefore = document.querySelector('.burger-menu__item-one');
+const burgerMenuItemCenter = document.querySelector('.burger-menu__item-two');
+const burgerMenuItemAfter = document.querySelector('.burger-menu__item-three');
 
 //Для окна авторизации
 const win = document.createElement('div');
@@ -29,8 +32,8 @@ winners.addEventListener('click', e => {
 authorLogin.addEventListener('click', createModalWin);
 authorReg.addEventListener('click', createModalWin);
 authorReg.addEventListener('click', e => {
-  authorReg.classList.add('active')
-})
+  authorReg.classList.add('active');
+});
 
 function createModalWin() {
 
@@ -46,49 +49,58 @@ function createModalWin() {
 
     winClose.addEventListener('click', () => {
       win.style.display = 'none';
-      authorReg.classList.remove('active')
+      authorReg.classList.remove('active');
     });
     winButtonClose.addEventListener('click', () => {
       win.style.display = 'none';
-      authorReg.classList.remove('active')
+      authorReg.classList.remove('active');
     });
-
   } else {
     win.style.display = 'block';
   };
 };
 
-// if (authorReg.style.display === none) {
-//   console.log('hello')
-// }
-// const header = document.querySelector('.header');
+burgerMenu.addEventListener('click', createMobileMenu);
+burgerMenu.addEventListener('click', rotate);
+burgerMenu.addEventListener('click', e => {
+  document.querySelector('.mobile-menu').classList.toggle('hidden')
+});
 
-// if (window.screen.availWidth <= 640) {
-//   header.insertAdjacentHTML("afterbegin", `
-//   <input id="menu__toggle" type="checkbox"/>
-//   <nav class = "menu">
-//     <a class="menu__item rules">Правила</a>
-//     <a class="menu__item prizes">Призы</a>
-//     <a class="menu__item winners">Победители</a>
-//   </nav>
-//   <div></div>
-//   <nav class="footer-menu">
-//     <a class="footer-menu__item footer-menu__connect" href="#">Обратная связь</a>
-//     <a class="footer-menu__item footer-menu__rules" href="#">Правила акции</a>
-//     <a class="footer-menu__item footer-menu__faq" href="#">faq</a>
-//   </nav>
-//   <div></div>
-//   <div class="social">
-//         <div class="social__title">Присоединяйтесь к нам</div>
-//         <div class="social-icons"> 
-//           <div class="social-icon icon__fb"></div>
-//           <div class="social-icon icon__vc"></div>
-//           <div class="social-icon icon__link"></div>
-//           <div class="social-icon icon__tw"></div>
-//           <div class="social-icon icon__inst"></div>
-//           <div class="social-icon icon__p"></div>
-//         </div>
-//       </div>
-//   <label class="menu__btn" for="menu__toggle">
-// `);
-// }
+function createMobileMenu () {
+  while (document.querySelector('.mobile-menu') <1) {
+    burgerMenu.insertAdjacentHTML("beforeend", `
+      <div class="mobile-menu hidden">
+        <nav class = "nav-menu">
+          <a class="nav-menu__item rules">Правила</a>
+          <a class="nav-menu__item prizes">Призы</a>
+          <a class="nav-menu__item winners">Победители</a>
+        </nav>
+        <div></div>
+        <nav class="info-mobile">
+          <a class="footer-menu__item footer-menu__connect" href="javascript:;">Обратная связь</a>
+          <a class="footer-menu__item footer-menu__rules" href="javascript:;">Правила акции</a>
+          <a class="footer-menu__item footer-menu__faq" href="javascript:;">faq</a>
+        </nav>
+        <div></div>
+        <div class="social-mobile">
+          <div class="social__title">Присоединяйтесь к нам</div>
+          <div class="social-icons"> 
+            <div class="social-icon icon__fb"></div>
+            <div class="social-icon icon__vc"></div>
+            <div class="social-icon icon__link"></div>
+            <div class="social-icon icon__tw"></div>
+            <div class="social-icon icon__inst"></div>
+            <div class="social-icon icon__p"></div>
+          </div>
+        </div>
+      </div>
+    `);
+  }
+}
+
+
+function rotate () {
+  burgerMenuItemCenter.classList.toggle('burger-menu__item-center');
+  burgerMenuItemBefore.classList.toggle('burger-menu__item-before');
+  burgerMenuItemAfter.classList.toggle('burger-menu__item-after');
+}
